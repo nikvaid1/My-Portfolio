28a3e2969b26425c8a662bd520f7fab76c2fddf3
nikvaid1 <nikhilvaid265@gmail.com>
2024-10-12 07:53:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Skills from "./components/sections/Skills";
 import StarCanvas from "./components/canvas/Stars";
 import { AnimatePresence } from "framer-motion";
 import Education from "./components/sections/Education";
-import Experience from "./components/sections/Experience";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import Footer from "./components/sections/Footer";
